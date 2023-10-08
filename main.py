9af8b60bf5b76fa1df49aa5622976edaf1643a4e
ALosee315 <77549989+ALosee315@users.noreply.github.com>
2023-10-07 20:28:00 -0400

    Began user init

--- main.py
+++ main.py
@@ -1,20 +1,4 @@
-import time
-
-print("Welcome back to Python")
-#test function that multiplies the first param by the second and returns the product
-def mult(num,num2):
-    var = num
-    var2 = num2
-    return num*num2
-result = mult(1,2)
-print(result)
-
-#Test out a while loop
-i = 0
-while i<10:
-  
-    result+=1
-    #test out the time.sleep function
-    time.sleep(0.5)
-    print(result+1)
-    i+=1
+print("Welcome to football sim 2023")
+print("What is your name?")
+name = input()
+print("Hello", name+"!")
